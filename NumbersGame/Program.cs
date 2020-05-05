97c14210b3130faa8b9a29ef5cf61bf02ee15365
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-04 19:02:44 -0500

    added getsum method

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -6,18 +6,45 @@ namespace NumbersGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            StartSequence();
+            try
+            {
+                Console.WriteLine("Hello World!");
+                StartSequence();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Danger, Will Robinson!");
+                Console.ReadLine();
+            }
+            finally
+            {
+                Console.WriteLine("The program is finished. Goodbye");
+                Console.ReadLine();
+            }
         }
         public static void StartSequence()
         {
-            Console.WriteLine("Welcome to my game! Lets do some math!");
-            Console.WriteLine("Please enter a number greater than zero.");
-            int userSize = Convert.ToInt32(Console.ReadLine());
-            int[] intialArray = new int[userSize];
-            Populate(intialArray);
-            Console.WriteLine("[{0}]", String.Join(", ", intialArray));
-            Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Welcome to my game! Lets do some math!");
+                Console.WriteLine("Please enter a number greater than zero.");
+                int userSize = Convert.ToInt32(Console.ReadLine());
+                int[] intialArray = new int[userSize];
+                Populate(intialArray);
+                Console.WriteLine("[{0}]", String.Join(", ", intialArray));
+                Console.WriteLine($"The sum is: {GetSum(intialArray)}");
+                Console.ReadLine();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Invalid format: {e}");
+                Console.ReadLine();
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine($"Overflow error: {e}");
+                Console.ReadLine();
+            }
         }
 
         public static int[] Populate(int[] array)
@@ -25,10 +52,21 @@ namespace NumbersGame
             int length = array.Length;
             for (int i = 0; i < length; i++)
             {
-                Console.Write($"Please enter a number, {(i +1)} of {length}: ");
+                Console.Write($"Please enter a number, {i + 1} of {length}: ");
                 array[i] = Convert.ToInt32(Console.ReadLine());
             }
             return array;
+        }
+
+        public static int GetSum(int[] array)
+        {
+            int sum = 0;
+            int length = array.Length;
+            for (int i = 0; i < length; i++)
+            {
+                sum = sum + array[i];
+            }
+            return sum;
         }
     }
 }
